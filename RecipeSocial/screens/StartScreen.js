1509a660091540d864c5a3a3b01637245a87a680
Dean Duraku <dean.duraku@hotmail.com>
2025-11-13 10:37:20 +0100

    story 1 mvp

--- RecipeSocial/screens/StartScreen.js
+++ RecipeSocial/screens/StartScreen.js
@@ -1,16 +1,22 @@
-import { View, Text, Button, Alert, TouchableOpacity } from 'react-native'
+import { View, Text, Button, Alert, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 
 export default function StartScreen() {
   return (
     <View className="flex-1 bg-white items-center justify-center px-6">
-      <Text className="text-4xl font-bold text-emerald-600 mb-10">
+       <Image
+        source={require('../assets/logo.png')}
+        className="w-80 h-80 rounded-2xl"
+                resizeMode="contain"
+       />
+
+      <Text className="text-4xl font-bold text--600 mb-10">
         RecipeSocial
       </Text>
 
       <TouchableOpacity
-        className="bg-emerald-500 w-full py-3 rounded-2xl mb-4"
-        onPress={() => Alert.alert('Simple Button pressed')}
+        className="bg-blue-500 w-full py-3 rounded-2xl mb-4"
+        onPress={() => navigation.navigate('Register')}
       >
         <Text className="text-white text-center text-lg font-semibold">
           Register
@@ -18,8 +24,8 @@ export default function StartScreen() {
       </TouchableOpacity>
 
       <TouchableOpacity
-        className="border border-emerald-500 w-full py-3 rounded-2xl"
-        onPress={() => Alert.alert('Log in pressed')}
+        className="border border-blue-500 w-full py-3 rounded-2xl"
+        onPress={() => navigation.navigate('Login')}
       >
         <Text className="text-emerald-600 text-center text-lg font-semibold">
           Log in
